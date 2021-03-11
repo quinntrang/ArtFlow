79307bd93b83396737d19bdcb86b23d3b9bcadb8
quinntrang <quinn.trang@pm.me>
2021-03-11 08:03:00 -0500

    Set up table view for current project screen

--- ArtFlow/ViewController.swift
+++ ArtFlow/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let tableView = UITableView()
+    var safeArea: UILayoutGuide!
+    
+    override func loadView() {
+        super.loadView()
+        safeArea = view.layoutMarginsGuide
+        
+        setupTableView()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemRed
 
         // Do any additional setup after loading the view.
+    }
+    
+    func setupTableView() {
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.topAnchor.constraint(equalTo: safeArea.topAnchor).isActive = true
+        tableView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        tableView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
     }
 
 

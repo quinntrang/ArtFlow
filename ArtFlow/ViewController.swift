c7754b6b1dfcdafa4e1ca659f9d2d8f5ec7eda26
quinntrang <quinn.trang@pm.me>
2021-03-11 09:13:12 -0500

    Populate table view on current project screen

--- ArtFlow/ViewController.swift
+++ ArtFlow/ViewController.swift
@@ -11,16 +11,18 @@ class ViewController: UIViewController {
     let tableView = UITableView()
     var safeArea: UILayoutGuide!
     
+    var characters = ["a", "b", "c"]
+    
     override func loadView() {
         super.loadView()
+        view.backgroundColor = .white
         safeArea = view.layoutMarginsGuide
         
         setupTableView()
     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemRed
-
         // Do any additional setup after loading the view.
     }
     
@@ -31,8 +33,28 @@ class ViewController: UIViewController {
         tableView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
         tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
         tableView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
 
+}
+
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return characters.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        //let cell = UITableViewCell()
+        
+        cell.textLabel?.text = characters[indexPath.row]
+        return cell
+    }
+    
 }
 
